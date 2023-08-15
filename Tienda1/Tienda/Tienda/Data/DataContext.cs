62f5866574cdd6e0f24f6628c5c467a6b703481b
RAMir0Z <r4mirodesp4@gmail.com>
2023-08-15 17:19:06 -0600

    Start Online Shop, but i have problems!! (mañana continuo xd)

---
+++ Tienda1/Tienda/Tienda/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tienda.Data.Entities;
+
+namespace Tienda.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
